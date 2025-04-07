1031568c63bc1525eca0703fea61e87631a3f324
MarceloVassallo <marcelo9733@gmail.com>
2025-04-07 10:36:18 -0500

    correcciones

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,11 +1,8 @@
 
-import ItemWarehouseForm from "@/components/ItemWarehouseForm";
-
 const Index = () => {
   return (
     <div>
-      <h1 className="text-2xl font-bold mb-8">Sistema de Gestión de Artículos en Almacén</h1>
-      <ItemWarehouseForm />
+      <h1 className="text-2xl font-bold mb-8">Siempresoft ERP</h1>
     </div>
   );
 };

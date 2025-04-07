4e1337bd9c5efe4ce8ee9d1fe27282f0069d8c57
MarceloVassallo <marcelo9733@gmail.com>
2025-04-07 12:33:56 -0500

    correccion

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,15 @@
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import Articles from "./pages/Articles";
 import EditWarehouse from "./pages/EditWarehouse";
-import ItemWarehouseForm from "./components/ItemWarehouseForm";
 import { LayoutWithSidebar } from "./components/LayoutWithSidebar";
 import ArticleSeries from "./pages/ArticleSeries";
 
-const queryClient = new QueryClient();
-
 const App = () => (
-  <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Toaster />
       <Sonner />
@@ -45,7 +40,6 @@ const App = () => (
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
-  </QueryClientProvider>
 );
 
 export default App;
